0f0cf1b1236a345e26a7b25d533581176c0628d3
shivam.choudhary <shivam.choudhary@girmiti.com>
2026-03-30 15:42:53 +0530

    user_status

--- userService/src/main/java/com/nector/userservice/controller/UserPermissionController.java
+++ userService/src/main/java/com/nector/userservice/controller/UserPermissionController.java
@@ -129,7 +129,7 @@ public class UserPermissionController {
         User updatedUser = userService.updateUser(userId, request);
 
         return ResponseEntity.ok(
-                Map.of("message", "User updated successfully", "username", updatedUser.getUsername())
+                Map.of("message", "User updated successfully", "username", updatedUser.getUsername(), "status", updatedUser.getStatus())
         );
     }
 

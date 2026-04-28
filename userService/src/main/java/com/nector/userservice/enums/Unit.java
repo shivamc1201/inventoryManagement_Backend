26ff84275cddbc93e1e5068a438c98a3545afb82
shivamc1201 <shivamchoudhary1201@gmail.com>
2026-04-29 01:56:49 +0530

    products_create_null check4.0

--- userService/src/main/java/com/nector/userservice/enums/Unit.java
+++ userService/src/main/java/com/nector/userservice/enums/Unit.java
@@ -3,8 +3,11 @@ package com.nector.userservice.enums;
 public enum Unit {
     KG("Kilogram"),
     LITER("Liter"),
+    LITRE("Litre"),
+    LITRES("Litres"),
     DOZEN("Dozen"),
     DOZENS("Dozens"),
+    PIECE("Piece"),
     PIECES("Pieces");
 
     private final String displayName;
